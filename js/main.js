3a288093cdf408558e235798470e02c8ca5679c2
T. Lanette <TLPollard4516@gmail.com>
2021-09-04 13:35:13 -0400

    Adding js for retrieving data
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -20,12 +20,21 @@ function closeMenu() {
 
 /************************** Planet Information **********/
 // Variables
+
+// Facts
 const planetName = document.querySelector(".navbar__header-title");
 const planetDesc = document.querySelector(".description-planet-text");
 const rotation = document.querySelector(".rotation-time ");
 const revolution = document.querySelector("revolution-time ");
 const radius = document.querySelector(".radius");
 const temperature = document.querySelector(".average-temp");
+
+//Buttons
+const overview = document.querySelector(".btn-overview");
+const structure = document.querySelector(".btn-structure");
+const geology = document.querySelector(".btn-geology");
+
+const vw = document.documentElement.clientWidth;
 
 
 // Objects for planet information
@@ -62,3 +71,58 @@ const planetSizes = {
     6: { large: "458px", medium: "290px", small: "176px" },
     7: { large: "450px", medium: "285px", small: "173px" }
 }
+
+
+const geologySize = {
+    large: "163px 199px",
+    medium: "80px 93.25px",
+    small: "50px 58.28px"
+}
+
+// Starting with first planet Mercury
+
+let currentPlanet = 0;
+let currentDesc = "overview";
+
+fetchData();
+changeBtn();
+
+function planetInput() {
+    currentPlanet = inputPlanet;
+    currentDesc = "overview";
+    fetchData();
+    changeBtn();
+}
+
+function overviewInput() {
+    currentDesc = "overview";
+    fetchData();
+    changeBtn();
+}
+
+function structureInput() {
+    currentDesc = "structure";
+    fetchData();
+    changeBtn();
+}
+
+function geologyInput() {
+    currentDesc = "geology";
+    fetchData();
+    changeBtn();
+}
+
+
+
+let data;
+
+function fetchData() {
+    if (typeof data === "undefined") {
+        fetch('data.json').then(response => response.json()).then(json => {
+            data = json;
+            displayInfo();
+        });
+    }
+    else displayInfo();
+}
+
