d756374d4005998f2a8620cf7de92ae9feb6dc67
T. Lanette <TLPollard4516@gmail.com>
2021-09-16 18:44:41 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -39,128 +39,29 @@ const overview = document.querySelector('.btn-overview');
 const structure = document.querySelector('.btn-structure');
 const geology = document.querySelector('.btn-geology');
 
+
 const vw = document.documentElement.clientWidth; // For window breakpoints
 
 
 
-// Objects for planet information
-const planets = {
-    0: 'mercury',
-    1: 'venus',
-    2: 'earth',
-    3: 'mars',
-    4: 'jupiter',
-    5: 'saturn',
-    6: 'uranus',
-    7: 'neptune'
-}
-
-// Colors based on planets
-const btnColors = {
-    0: '419EBB',
-    1: 'EDA249',
-    2: '6D2ED5',
-    3: 'D14C32',
-    4: 'D83A34',
-    5: 'CD5120',
-    6: '1EC1A2',
-    7: '2D68F0'
-}
-
-// Planet image sizes by breakpoint
-const planetSize = {
-    0: { large: "290px", medium: "184px", small: "111px"},
-    1: { large: "400px", medium: "253px", small: "154px" },
-    2: { large: "450px", medium: "285px", small: "173px" },
-    3: { large: "336px", medium: "213px", small: "129px" },
-    4: { large: "582px", medium: "369px", small: "224px" },
-    5: { large: "666.09px", medium: "422px", small: "256px" },
-    6: { large: "458px", medium: "290px", small: "176px" },
-    7: { large: "450px", medium: "285px", small: "173px" }
-}
-
-// Geology images sizes
-const geologySize = {
-    large: "163px 199px",
-    medium: "80px 93.25px",
-    small: "50px 58.28px"
-}
-
-let currentPlanet = planets[0];
-let currentState = "overview";
-
-fetchData();
-changeBtn();
-
+var data;
 
 // Get information from data.json
-let data;
 function fetchData() {
-    fetch('data.json').then(response => {
-        if (!response.ok) {
-            throw Error("ERROR");
-        }
-        return response.json();
-    })
-        .then(json => {
-            data = json;
-            //console.log(data);
-            displayPlanetInfo();
-        });
-    
+    if (typeof data === 'undefined') {
+        fetch('data.json').then(response => {
+            if (!response.ok) {
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+            .then(json => {
+                data = json;
+                console.log(data);
+                //displayPlanetInfo();
+            });
+    }
+    //else displayPlanetInfo();
 }
-
 fetchData();
 
-
-// Change buttons to change data 
-function changeBtn() {
-    if (currentState === "overview") {
-        overview.style.backgroundColor = `${btnColors[currentPlanet]}`;
-        structure.style.backgroundColor = 'transparent';
-        geology.style.backgroundColor = 'transparent';
-    } else if (currentState === "structure") {
-        overview.style.backgroundColor = 'transparent';
-        structure.style.backgroundColor = `${btnColors[currentPlanet]}`;
-        geology.style.backgroundColor = 'transparent';
-    } else {
-        overview.style.backgroundColor = 'transparent';
-        structure.style.backgroundColor = 'transparent';
-        geology.style.backgroundColor = `${btnColors[currentPlanet]}`;
-    }
-}
-
-changeBtn();
-
-
-function overviewBtn() {
-    currentState = "overview"
-    fetchData();
-    changeBtn();
-}
-
-function structureBtn() {
-    currentState = "structure"
-    fetchData();
-    changeBtn();
-}
-
-function geologyBtn() {
-    currentState = "geology"
-    fetchData();
-    changeBtn();
-}
-
-
-
-
-function displayPlanetInfo() {
-    /*planetName.innerText = data[currentPlanet].name;
-    rotation.innerText = data[currentPlanet].rotation;
-    revolution.innerText = data[currentPlanet].revolution;
-    radius.innerText = data[currentPlanet].radius;
-    temperature.innerText = data[currentPlanet].temperature;
-    console.log(planetName);*/
-}
-
-displayPlanetInfo();
--- project.md
+++ project.md
@@ -162,3 +162,47 @@ Radius
 
 Average Temp.
 471°c
+
+
+// Objects for planet information
+const planets = {
+    0: 'mercury',
+    1: 'venus',
+    2: 'earth',
+    3: 'mars',
+    4: 'jupiter',
+    5: 'saturn',
+    6: 'uranus',
+    7: 'neptune'
+}
+
+// Colors based on planets
+const btnColors = {
+    0: '419EBB',
+    1: 'EDA249',
+    2: '6D2ED5',
+    3: 'D14C32',
+    4: 'D83A34',
+    5: 'CD5120',
+    6: '1EC1A2',
+    7: '2D68F0'
+}
+
+// Planet image sizes by breakpoint
+const planetSize = {
+    0: { large: "290px", medium: "184px", small: "111px"},
+    1: { large: "400px", medium: "253px", small: "154px" },
+    2: { large: "450px", medium: "285px", small: "173px" },
+    3: { large: "336px", medium: "213px", small: "129px" },
+    4: { large: "582px", medium: "369px", small: "224px" },
+    5: { large: "666.09px", medium: "422px", small: "256px" },
+    6: { large: "458px", medium: "290px", small: "176px" },
+    7: { large: "450px", medium: "285px", small: "173px" }
+}
+
+// Geology images sizes
+const geologySize = {
+    large: "163px 199px",
+    medium: "80px 93.25px",
+    small: "50px 58.28px"
+}
